e6843a27e30463600bec8e1a83218293d244fe59
estebanmmpa <esteban.piga.developer@gmail.com>
2025-11-29 01:14:08 -0300

    Fix table_name environment variable

--- backend/src/common/config.ts
+++ backend/src/common/config.ts
@@ -1,3 +1,9 @@
+/**
+ * REMEMBER:
+ * SAM does not automatically read from a .env file for Lambdas (even locally).
+ * We must provide all environment variables explicitly in the template.yaml 
+ * or pass them via the CLI when running locally.
+ */
 export const Configuration = {
     ApiBaseUrl: process.env.API_BASE_URL,
     DynamoDbTable: process.env.TABLE_NAME
